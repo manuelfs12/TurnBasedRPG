279ad3cbab5707338790c9fdd44ebfc13fc3740b
Manuel Figueroa <manuelfigueroa402@gmail.com>
2020-04-09 04:41:44 -0400

    Player movement without the sprite

---
+++ source/PlayState.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.text.FlxText;
+import flixel.FlxState;
+
+class PlayState extends FlxState {
+	var player:Player;
+
+	override public function create() {
+		player = new Player(20, 20);
+		add(player);
+		super.create();
+	}
+
+	override public function update(elapsed:Float) {
+		super.update(elapsed);
+	}
+}
