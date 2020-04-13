5489d2629de6c369247c7dc7d450f43f3a55e6ec
Manuel Figueroa <manuelfigueroa402@gmail.com>
2020-04-13 11:27:14 -0400

    added desktop conditionals

--- source/Main.hx
+++ source/Main.hx
@@ -7,10 +7,18 @@ import openfl.display.Sprite;
 
 class Main extends Sprite {
 	public function new() {
-		super();
-		addChild(new FlxGame(320, 240, MenuState));
-		var save = new FlxSave();
+		var startFullscreen:Bool = false;
+		var save:FlxSave = new FlxSave();
 		save.bind("TurnBasedRPG");
+		#if desktop
+		if (save.data.fullscreen != null) {
+			startFullscreen = save.data.fullscreen;
+		}
+		#end
+
+		super();
+		addChild(new FlxGame(320, 240, MenuState, 1, 60, 60, false, startFullscreen));
+
 		if (save.data.volume != null) {
 			FlxG.sound.volume = save.data.volume;
 		}
--- source/MenuState.hx
+++ source/MenuState.hx
@@ -10,6 +10,10 @@ class MenuState extends FlxState {
 	var titleText:FlxText;
 	var optionsButton:FlxButton;
 
+	#if desktop
+	var exitButton:FlxButton;
+	#end
+
 	override public function create() {
 		titleText = new FlxText(20, 0, 0, "HaxeFlixel\nTutorial\nGame", 22);
 		titleText.alignment = CENTER;
@@ -27,6 +31,12 @@ class MenuState extends FlxState {
 		optionsButton.x = (FlxG.width / 2) + 10;
 		optionsButton.y = FlxG.height - optionsButton.height - 10;
 		add(optionsButton);
+
+		#if desktop
+		exitButton = new FlxButton(FlxG.width - 28, 8, "X", clickExit);
+		exitButton.loadGraphic(AssetPaths.button__png, true, 20, 20);
+		add(exitButton);
+		#end
 
 		if (FlxG.sound.music == null) // don't restart the music if it's already playing
 		{
@@ -46,4 +56,10 @@ class MenuState extends FlxState {
 	function clickOptions() {
 		FlxG.switchState(new OptionsState());
 	}
+
+	#if desktop
+	function clickExit() {
+		Sys.exit(0);
+	}
+	#end
 }
